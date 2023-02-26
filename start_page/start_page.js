3c5f5f7ddc52555189fdf1df73f28b72c14c0111
BayzU <bayzedullah@gmail.com>
2023-02-25 21:03:24 -0500

    Hover effect on start-btn

--- start_page/start_page.js
+++ start_page/start_page.js
@@ -3,3 +3,15 @@ const startButton = document.getElementById("start-btn")
 startButton.addEventListener('click', () => {
     window.location.href ="../categories-pw2/categories.html"
 })
+
+startButton.addEventListener('mouseover', () => {
+    startButton.style.backgroundColor = "#142455"
+    startButton.style.width = "155px"
+    startButton.style.height = "55px"
+})
+
+startButton.addEventListener('mouseout', () => {
+    startButton.style.backgroundColor = ""
+    startButton.style.width = ""
+    startButton.style.height = ""
+})
