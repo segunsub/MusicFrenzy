e7dd4ca5c1f78440101bb9fd8676250daa0a1303
sodiqlab <henryymustapha@gmail.com>
2023-03-02 02:50:27 -0500

    revised

--- musicly.js
+++ musicly.js
@@ -6,11 +6,19 @@ const restart= document.querySelector(".restart")
 const startButton = document.getElementById("start-btn")
 const categoryBtn = document.querySelectorAll(".btn-style")
 const categoryTitle = document.querySelector(".category-title")
+const again= document.querySelector('.arrow')
+const restartButton = document.querySelector('.arrow');
+
+
 const quizOptions = document.querySelectorAll(".song-choices")
 let arr= []
-const chosenSong= ''
+let chosenSong= ''
 let picked= ''
-let song= ''
+let songs= ''
+let right= ''
+let randomarr= []
+let round = 1;
+let cat= ''
 const genre = {
     "Hip Hop": {
         "Humble": "/Resources/rema.mp3",
@@ -47,10 +55,11 @@ const genre = {
 };
 
 startButton.addEventListener('click', () => {
-    startPage.style.display = "none";
-    categoriesPage.style.display = "flex";
-})
-
+   // resetGame();
+    startPage.style.display = 'none';
+    categoriesPage.style.display = 'flex';
+  });
+  
 startButton.addEventListener('mouseover', () => {
     startButton.style.backgroundColor = "#142455"
     startButton.style.width = "155px"
@@ -68,27 +77,13 @@ restart.addEventListener('click', () => {
     
 })
 
-changeText();
 
-function changeText() {
-    categoryBtn.forEach(node => {
-        node.addEventListener('click', () => {
-            categoriesPage.style.display = "none";
-            quizPage.style.display = "flex";
-            categoryTitle.innerText = `${node.innerText}`;
-            pickRandom();
-            fillIn(); 
-            correctChoice();
-            song = './Resources/' + correctChoice() + '.mp3';
-            playSong(`${song}`) 
-            pickedSong()
-            isItCorrect()
-            
-
-            //playSong("./Resources")
-        })
+function undisableButtons() {
+    quizOptions.forEach(button => {
+      button.disabled = false;
     })
-}
+  }
+  
 
 function getGenre() {
     return categoryTitle.innerText
@@ -97,32 +92,52 @@ function getGenre() {
 getGenre()
 
 function getMusicList() {
-    const songs = genre[getGenre()];
-    arr= Object.keys(songs);
+    songs = genre[getGenre()];
+    arr = Object.keys(songs);
+    randomarr = Object.keys(songs);
     return Object.keys(songs);
 };
 
 function playSong(songFile) {
     const audio = new Audio(songFile);
     audio.play();
+    return audio;
 }
+
+let currentAudio;
+
+function changeSong() {
+    const song = './Resources/' + correctChoice() + '.mp3';
+    if (currentAudio) {
+        currentAudio.pause();
+    }
+    currentAudio = playSong(song);
+}
+
 
 function fillIn() {
     let counter = 0;
+    const options = getMusicList();
+    // Fisher-Yates shuffle
+    for (let i = options.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [options[i], options[j]] = [options[j], options[i]];
+    }
     quizOptions.forEach(button => {
-        let music = getMusicList()[counter];
+        let music = options[counter];
         button.innerText = `${music}`;
         counter++;
         if (counter === 4) return;
     })
 }
 
-function pickRandom() {
-    const randomIndex = Math.floor(Math.random() * getMusicList().length);
-    const song = arr[randomIndex];
-    return song;
-}
-let right= ''
+
+// function pickRandom() {
+//     const randomIndex = Math.f   loor(Math.random() * getMusicList().length);
+//     const song = arr[randomIndex];
+//     return song;
+// }
+
 function correctChoice(){
     const randomCorrect= Math.floor(Math.random() * arr.length )
     right= arr[randomCorrect]
@@ -130,37 +145,137 @@ function correctChoice(){
 }
 
 function pickedSong() {
-    let flag = false
+    let flag = false;
     quizOptions.forEach(button => {
-        button.setAttribute("id", button.innerText)
-        button.addEventListener('click', () => {
-        picked= `${button.innerText}`
-        if (picked=== right){
-            button.style.backgroundColor= 'green'
-            document.getElementById('score-number').innerText = '100'
-            quizOptions.forEach(button => {
-                if (button.id !== picked) button.disabled = true
-            })
-        }else{
-            button.style.backgroundColor= 'blue'
-            button.style.backgroundColor= 'red'
-            quizOptions.forEach(button => {
-                if (button.id !== picked) button.disabled = true
-            })
+      button.setAttribute('id', button.innerText);
+      button.addEventListener('click', () => {
+        picked = `${button.innerText}`;
+        if (picked === right) {
+          button.style.backgroundColor = 'green';
+          counter--;
+          document.getElementById('score-number').innerText = counter * 25;
+          if (counter === 0) {
+            setTimeout(() => {
+              quizPage.style.display = 'none';
+              finalPage.style.display = 'flex';
+              document.getElementById('final-score').innerText = counter * 25;
+            }, 1000);
+          }
+          quizOptions.forEach(button => {
+            if (button.id !== picked) button.disabled = true;
+          });
+        } else {
+          button.style.backgroundColor = 'red';
+          quizOptions.forEach(button => {
+            if (button.id !== picked) button.disabled = true;
+          });
+        //   if (counter === 0) {
+        //     setTimeout(() => {
+        //       quizPage.style.display = 'none';
+        //       finalPage.style.display = 'flex';
+        //       document.getElementById('final-score').innerText = counter * 25;
+        //     }, 1000);
+        //   }
         }
-        })  
-    })
+      });
+    });
+  }
   
-    
-}
-
 
 function isItCorrect(){
+    counter= 0
+    button.style.backgroundColor= 'green'
     if(`${button.innerText}` === right){
         button.style.backgroundColor = 'green'
         
     }else{
         button.style.backgroundColor= 'blue'
-        button.style.backgroundColor= 'green'
+        
     }
 }
+//let counter = 4
+
+function resetGame() {
+    let redocounter=4
+    //let optionsredo = getMusicList();
+    //arr = [];
+    chosenSong = '';
+    picked = '';
+    songs = '';
+    right = '';
+    //randomarr = [];
+    categoryTitle.innerText = cat
+    for (let i = options.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [optionsredo[i], optionsredo[j]] = [optionsredo[j], optionsredo[i]];
+    }
+    quizOptions.forEach(button => {
+      button.style.backgroundColor = '';
+      button.disabled = false;
+      let music = options[redocounter];
+        button.innerText = `${music}`;
+        redocounter--;
+        if (counter === 0) return;
+    });
+  }
+
+  restartButton.addEventListener('click', () => {
+  resetGame();
+});
+  function fillIn() {
+    let counter = 0;
+    const options = getMusicList();
+    // Fisher-Yates shuffle
+    for (let i = options.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [options[i], options[j]] = [options[j], options[i]];
+    }
+    quizOptions.forEach(button => {
+        let music = options[counter];
+        button.innerText = `${music}`;
+        counter++;
+        if (counter === 4) return;
+    })
+}
+changeText();
+
+function changeText() {
+    categoryBtn.forEach(node => {
+        node.addEventListener('click', () => {
+            categoriesPage.style.display = "none";
+            quizPage.style.display = "flex";
+            categoryTitle.innerText = `${node.innerText}`;
+            cat= `${node.innerText}`;
+        })
+    })
+}
+
+// function changeText() {
+//     categoryBtn.forEach(node => {
+//         node.addEventListener('click', () => {
+//             if (round > 4) {
+//                 // End the game if we've played 4 rounds
+//                 quizPage.style.display = "none";
+//                 finalPage.style.display = "flex";
+//             } else {
+//                 categoriesPage.style.display = "none";
+//                 quizPage.style.display = "flex";
+//                 categoryTitle.innerText = `${node.innerText}`;
+//                 cat= `${node.innerText}`;
+//                 fillIn(); 
+//                 correctChoice();
+//                 song = './Resources/' + correctChoice() + '.mp3';
+//                 playSong(`${song}`) 
+//                 pickedSong()
+//                 quizOptions.forEach(button => {
+//                     let music = options[counter];
+//                     button.innerText = `${music}`;
+//                     counter++;
+//                     if (counter === 4) return;
+//                 })
+//                 round++; // Increment the round number
+                
+//             }
+//         })
+//     })
+// }
