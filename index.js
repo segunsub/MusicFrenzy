a983dabde6cfd5e625e4c0db3fd097a9cca7f8c4
segunsub <segunsubair3@gmail.com>
2023-03-15 16:33:45 -0400

    update to styles

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from './dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 const startPage = document.querySelector(".start-page");
 const categoriesPage = document.querySelector(".categories-page");
